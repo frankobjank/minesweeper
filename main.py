5c0b85021cbb287d8fbedaeab22f4118f4191aae
Jacob Frank <frankobjank@gmail.com>
2023-08-21 00:16:12 -0400

    Added reset button and function for reseting the game as well as fixing up end game rendering. Need to add adjacent square function + loop next

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from pyray import *
 # for incorporating medium and hard, start with menu to choose difficulty
 # then use set_window_size based on selection
 # globals
-MOUSE_BUTTON_LEFT= 0
-MOUSE_BUTTON_RIGHT= 1
-MOUSE_BUTTON_MIDDLE= 2
+mouse_button_left= 0
+mouse_button_right= 1
+key_m= 77
 
 block = 30
 header_height=block*2
 screen_width=block*10
 screen_height=block*10+header_height
 num_mines = 10
-reset_button = Vector2(screen_width//2-block*2, block//2)
+reset_button = Rectangle(screen_width//2-block, block//2, block*2, block,)
+
+def get_random_coords():
+    return Vector2(random.randrange(0, screen_width, 30), random.randrange(header_height, screen_height, 30))
 
 class Square:
     def __init__(self, x, y):
@@ -24,14 +27,33 @@ class Square:
         self.mine = False
         self.flag = False
         self.visible = False
+        self.rectangle = Rectangle(self.x, self.y, block, block)
+        self.blow_up = False
+
+    def __repr__(self):
+        return f"({self.x}, {self.y}), mine = {self.mine}, flag = {self.flag}, visible = {self.visible}"
+    
+    def get_adjacent(self, state):
+        return [state.board[(x, y)] for y in range(-block, 0, block) for x in range(-block, 0, block)]
 
 class State:
     def __init__(self):
         self.board = {}
+        self.board_rectangle = Rectangle(0, header_height, screen_width, screen_height)
         self.mines = set()
         self.visible_squares = set()
         self.flags = set()
         self.selection = None
+        self.lose = False
+        self.win = False
+        self.reset = False
+
+    
+
+def reset():
+    new_state = State()
+    create_board(new_state, num_mines, fixed_mines=False)
+    return new_state
 
 
 def create_board(state, num_mines, fixed_mines=False):
@@ -43,13 +65,11 @@ def create_board(state, num_mines, fixed_mines=False):
             state.board[(mine)].mine = True
     # random mines
     else:
-        # mines = set() # local set to get up to 10 and exclude dupes
         while len(state.mines)<num_mines:
-            mine = (get_random_value(0, screen_width), get_random_value(0, screen_height))
-            # mines.add(mine)
-            state.mines.add(state.board[(mine)]) # global mines
-            state.board[(mine)].mine = True
-    
+            mine = (get_random_coords())
+            state.mines.add(state.board[(mine.x, mine.y)])
+            state.board[(mine.x, mine.y)].mine = True
+
     # # calc adj to mines
     # for mine in state.mines:
     #     # print((mine.x, mine.y)) # for debugging
@@ -57,69 +77,140 @@ def create_board(state, num_mines, fixed_mines=False):
     #     for adj in all_adjacent:
     #         adj.adj += 1
 
+# could change all squares to Rectangles if you can get them to be structs
+# easier to calculate collision if no modulo
+
+
+# check_collision_point_rec(point: Vector2, rec: Rectangle)
+
 
 def update(state):
 
-    if is_mouse_button_down(0) or is_mouse_button_down(1):
+    if is_mouse_button_down(mouse_button_left): # hold down left click
         state.selection = get_mouse_position()
-        state.selection.x -= state.selection.x % 30
-        state.selection.y -= state.selection.y % 30
-        state.selection = state.board.get((state.selection.x, state.selection.y), None)
-        if state.selection.visible == True or state.selection.flag == True:
-            state.selection = None  
-
-    elif is_mouse_button_released(0):
-        if state.selection:
-            state.selection.visible = True
-            state.visible_squares.add(state.selection)
+        # check if selection is on board and game isn't over
+        if check_collision_point_rec(state.selection, state.board_rectangle) == True and state.lose == False and state.win == False:
+            state.selection.x -= state.selection.x % 30
+            state.selection.y -= state.selection.y % 30
+            state.selection = state.board.get((state.selection.x, state.selection.y), None)
+            if state.selection.visible == True or state.selection.flag == True: # if flag or visible
+                state.selection = None
+        # check if selection on reset button, works even if game is over
+        elif check_collision_point_rec(state.selection, reset_button) == True:
+            state.selection = "reset"
+        else:
             state.selection = None
 
-    elif is_mouse_button_released(1):
+
+    elif is_mouse_button_down(mouse_button_right): # hold down right click 
+        state.selection = get_mouse_position()
+        # check if selection on board and game isn't over
+        if check_collision_point_rec(state.selection, state.board_rectangle) == True and state.lose == False and state.win == False:
+            state.selection.x -= state.selection.x % 30
+            state.selection.y -= state.selection.y % 30
+            state.selection = state.board.get((state.selection.x, state.selection.y), None)
+            if state.selection.visible == True: # (allows selection of flags)
+                state.selection = None
+        # check if selection on reset button, works even if game is over
+        elif check_collision_point_rec(state.selection, reset_button) == True:
+            state.selection = "reset"
+        else:
+            state.selection = None
+
+    elif is_mouse_button_released(mouse_button_left): # release left click
+        if state.selection == "reset":
+            state.reset = True
+            return
+        elif state.selection != None:
+            if state.selection.flag == False: # if not a flag
+                if state.selection.mine == True:
+                    state.lose = True
+                    state.selection.blow_up = True
+                elif state.selection.adj > 0:
+                    pass
+                else: # if revealing a number
+                    state.selection.visible = True
+                    state.visible_squares.add(state.selection)
+                    state.selection = None
+
+    elif is_mouse_button_released(mouse_button_right): # release right click
+        if state.selection == "reset":
+            state.reset = True
+            return
         if state.selection != None and state.selection.visible == False:
             if state.selection.flag == False:
                 state.selection.flag = True
                 state.flags.add(state.selection)
                 state.selection = None
-            else:
+
+            elif state.selection.flag == True:
                 state.selection.flag = False
                 state.flags.remove(state.selection)
                 state.selection = None
-
-
+    
+    # for debugging - press m to reveal
+    if is_key_pressed(key_m):
+        print(state.reset)
 
 def render(state):
     begin_drawing()
     clear_background(LIGHTGRAY)
+    # draw squares in board
     for square in state.board.values():
-        if square == state.selection:
-            draw_rectangle(state.selection.x, state.selection.y, 30, 30, LIGHTGRAY)
-        elif square.flag == True:
+        if square.flag == True: # flags don't change until deselected
             draw_rectangle(square.x, square.y, 30, 30, BLUE)
+        elif square == state.selection:
+            draw_rectangle(state.selection.x, state.selection.y, 30, 30, LIGHTGRAY)
         elif square.visible == True:
-            if square.mine == True:
-                draw_rectangle(square.x, square.y, 30, 30, RED)
-            else:
-                draw_rectangle(square.x, square.y, 30, 30, GREEN)
+            draw_rectangle(square.x, square.y, 30, 30, GREEN)
+        # debugging - display where mines are
+        # elif square.mine == True:
+        #     draw_rectangle(square.x, square.y, 30, 30, RED)
         else:
             draw_rectangle(square.x, square.y, 30, 30, DARKGRAY)
+    
+    if state.lose == True:
+        for mine in state.mines:
+            draw_rectangle(mine.x, mine.y, 30, 30, RED)
+            if mine.blow_up == True:
+                draw_rectangle(mine.x, mine.y, 30, 30, ORANGE)
+    elif state.win == True:
+        pass
+    
+    draw_grid()
 
+    # draw reset button
+    draw_rectangle(int(reset_button.x), int(reset_button.y), block*2, block, GRAY)
+    draw_text("Reset", screen_width//2-block+5, 20, 19, BLACK)
+    end_drawing()
+
+
+def render_win(state):
+    pass
+
+def draw_grid():
     for x in range(0, screen_width, block):
         draw_line(x, header_height, x, screen_height, BLACK)
     for y in range(header_height, screen_height, block):
         draw_line(0, y, screen_width, y, BLACK)
-    draw_rectangle_v(reset_button, reset_button, GRAY)
-    end_drawing()
 
-state = State()
 
-def main(state):
+def main():
+    state = State()
     set_target_fps(60)
-    create_board(state, num_mines, fixed_mines=True)
+    create_board(state, num_mines, fixed_mines=False)
     init_window(screen_width, screen_height, "Minesweeper")
     while not window_should_close():
         update(state)
         render(state)
 
+        # reset game after win or lose
+        if state.reset == True:
+            state = reset()
     close_window()
 
-main(state)
+main()
+
+# track time
+# save high-scores
+# display mines left
