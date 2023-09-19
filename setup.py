e6c68728c71f2242b6fe69c05267343b43df1303
Jacob Frank <frankobjank@gmail.com>
2023-09-19 01:26:11 -0400

    added date to setup name

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
+from datetime import date
+
 APP =["main.py"]
 OPTIONS = {
     'argv_emulation': True,
 }
 
 setup(
-    name = "Minesweeper",
+    name = f"Minesweeper_{date.today()}",
     app = APP,
     options={"py2app": OPTIONS},
     setup_requires=["py2app"]
